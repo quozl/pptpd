quozl/pptpd

commit 1dbf25a5b10df5abc16baf1095086100838ff34b

bcrelay: added save daemon PID to file
Signed-off-by: Brilliantov Kirill Vladimirovich <brilliantov@inbox.ru>
[style and consistency]
Reviewed-by: James Cameron <quozl@laptop.org>

defaults.h
@@ -44,6 +44,9 @@
 #define PPTPD_CONFIG_FILE_DEFAULT       "/etc/pptpd.conf"
 #endif
 #define PIDFILE_DEFAULT                 "/var/run/pptpd.pid"
+#ifdef BCRELAY
+#define PIDFILE_BCRELAY_DEFAULT         "/var/run/bcrelay.pid"
+#endif
 
 #define STIMEOUT_DEFAULT                10 /* seconds */
 

README.bcrelay
@@ -39,6 +39,7 @@ to the outgoing interface.
 Usage: /usr/sbin/bcrelay [options], where options are:
 
  [-d] [--daemon]           Run as daemon.
+ [-p] [--pidfile]          Write process ID to file if run as daemon.
  [-h] [--help]             Displays this help message.
  [-i] [--incoming]         Defines from which interface broadcasts will be
                            relayed.

bcrelay.8
@@ -15,6 +15,10 @@ interface.
 \fB-d\fR,\fB\-\-daemon\fR
 Run \fBbcrelay\fR as a daemon.
 .TP
+\fB-p\fR,\fB\-\-pidfile\fR \fIpid-file
+specifies an alternate location to store the process ID file (default
+.IR /var/run/bcrelay.pid ).
+.TP
 \fB-h\fR,\fB\-\-help\fR
 Show help message.
 .TP

bcrelay.c
@@ -237,6 +237,9 @@ static void showusage(char *prog)
   printf(" for broadcast packets. When it receives the packets, it will then re-broadcast the packet.\n\n");
   printf("Usage: %s [options], where options are:\n\n", prog);
   printf(" [-d] [--daemon]           Run as daemon.\n");
+  printf(" [-p] [--pidfile <file>]   Write process ID to file if run as daemon.\n");
+  printf("                           (default is %s).\n",
+         PIDFILE_BCRELAY_DEFAULT);
   printf(" [-h] [--help]             Displays this help message.\n");
   printf(" [-i] [--incoming <ifin>]  Defines from which interface broadcasts will be relayed.\n");
   printf(" [-n] [--nolog]            No logging/tracing to /var/log/messages.\n");
@@ -325,6 +328,7 @@ int main(int argc, char **argv) {
   int c;
   char *ifout = empty;
   char *ifin = empty;
+  char *pidfile = empty;
 
 #ifndef BCRELAY
   fprintf(stderr,
@@ -343,6 +347,7 @@ int main(int argc, char **argv) {
       {
         {"nolog", 0, 0, 0},
         {"daemon", 0, 0, 0},
+        {"pidfile", 1, 0, 0},
         {"help", 0, 0, 0},
         {"incoming", 1, 0, 0},
         {"outgoing", 1, 0, 0},
@@ -351,18 +356,21 @@ int main(int argc, char **argv) {
         {0, 0, 0, 0}
       };
 
-    c = getopt_long(argc, argv, "ndhi:o:s:v", long_options, &option_index);
+    c = getopt_long(argc, argv, "ndp:hi:o:s:v", long_options, &option_index);
     if (c == -1)
       break;
     /* convert long options to short form */
     if (c == 0)
-      c = "ndhiosv"[option_index];
+      c = "ndphiosv"[option_index];
     switch (c) {
     case 'n':
       vnologging = 1;
       break;
     case 'd':
       vdaemon = 1;
+      break;
+    case 'p':
+      pidfile = strdup(optarg);
       break;
     case 'h':
       showusage(argv[0]);
@@ -412,6 +420,7 @@ int main(int argc, char **argv) {
 
   // If specified, become Daemon.
   if (vdaemon) {
+    FILE *fd;
 #if HAVE_DAEMON
     closelog();
     if (freopen("/dev/null", "r", stdin) == NULL) {
@@ -430,10 +439,23 @@ int main(int argc, char **argv) {
      * never returns if HAVE_FORK (re-execs without -d)
      */
 #endif
+    if (pidfile == empty)
+      pidfile = PIDFILE_BCRELAY_DEFAULT;
+    fd = fopen(pidfile, "w");
+    if (fd != NULL) {
+      fprintf(fd, "%lu\n", getpid());
+      fclose(fd);
+    } else {
+      syslog(LOG_ERR, "failed to open pidfile %s\n", pidfile);
+    }
   } else {
     syslog(LOG_INFO, "Running as child\n");
   }
   mainloop(argc,argv);
+  if (vdaemon) {
+    if (unlink(pidfile))
+      syslog(LOG_ERR, "failed to remove pidfile %s\n", pidfile);
+  }
   _exit(0);
 }
 

pptpd.c
@@ -115,7 +115,7 @@ static void showusage(char *prog)
         printf("                           (default is /etc/ppp/options).\n");
 #endif
         printf(" [-p] [--pidfile file]     Specifies the file to write the process ID to\n");
-        printf("                           (default is /var/run/pptpd.pid).\n");
+        printf("                           (default is %s).\n", PIDFILE_DEFAULT);
 #if !defined(BSDUSER_PPP)
         printf(" [-s] [--speed baud]       Specifies the baud speed for the PPP daemon\n");
         printf("                           (default is 115200).\n");

commit 3b7a80c3b63a9f9388991299f76c245bb900875e

Fix build without --enable-bcrelay
When built without --enable-bcrelay,

bcrelay.c: In function 'showusage':
bcrelay.c:242:10: error: 'PIDFILE_BCRELAY_DEFAULT' undeclared (first use in this function)
          PIDFILE_BCRELAY_DEFAULT);
          ^~~~~~~~~~~~~~~~~~~~~~~

Regression introduced by 1dbf25a5b10df5abc16baf1095086100838ff34b.

Reported-by: mattias jonsson <mj@mjw.se>
Signed-off-by: James Cameron <quozl@laptop.org>

defaults.h
@@ -44,9 +44,7 @@
 #define PPTPD_CONFIG_FILE_DEFAULT       "/etc/pptpd.conf"
 #endif
 #define PIDFILE_DEFAULT                 "/var/run/pptpd.pid"
-#ifdef BCRELAY
 #define PIDFILE_BCRELAY_DEFAULT         "/var/run/bcrelay.pid"
-#endif
 
 #define STIMEOUT_DEFAULT                10 /* seconds */
 